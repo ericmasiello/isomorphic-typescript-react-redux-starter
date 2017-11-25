cf71eedc9d036e9c16728e11e69208b89a7c4695
Eric Masiello <eric.j.masiello@gmail.com>
2017-11-25 18:24:55 -0500

    Move types to global.d.ts file

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { AuthToken } from '../../types';
 
 interface Props {
   auth: AuthToken;
---
+++ src/global.d.ts
@@ -0,0 +1,3 @@
+interface AuthToken {
+  googleId: string;
+}
