e14a28ad07da53e136f4e7671273929ca57dfcd4
Eric Masiello <eric.j.masiello@gmail.com>
2017-11-26 09:36:23 -0500

    Add source maps

--- webpack.base.js
+++ webpack.base.js
@@ -9,6 +9,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.json', '.ts', '.tsx'],
   },
+  devtool: 'source-map',
   // Tell webpack to run babel on every file it runs through
   module: {
     rules: [
