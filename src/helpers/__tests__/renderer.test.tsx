0a97f4e7f2cb2400501af52ec3da9f4849896377
Eric Masiello <eric.j.masiello@gmail.com>
2017-12-02 08:59:51 -0500

    Add test for renderer.

--- src/helpers/__tests__/renderer.test.tsx
+++ src/helpers/__tests__/renderer.test.tsx
@@ -1,1 +1,19 @@
-test('testing...', () => expect(true).toBe(true));
+import renderer from '../renderer';
+import * as express from 'express';
+import createStore from '../createStore';
+jest.mock('react-helmet', () => {
+  const obj: any = { Helmet: {} };
+  obj.Helmet.renderStatic = jest.fn(() => ({
+    title: { toString: jest.fn(() => 'The title') },
+    meta: { toString: jest.fn(() => 'The meta') },
+  }));
+  return obj;
+});
+
+test('should render an html string', () => {
+  const req = {} as express.Request;
+  req.get = jest.fn();
+  const store = createStore(req);
+  const result = renderer(req, store, {});
+  expect(result).toMatchSnapshot();
+});
