68b713958a31b30ca1fe9d68565e5813a4c097b7
Eric Masiello <eric.j.masiello@gmail.com>
2017-12-03 15:42:00 -0500

    Reorganize code

--- src/client/actions/index.ts
+++ src/client/actions/index.ts
@@ -2,21 +2,17 @@ import { ThunkActionCreator } from '../../types.d';
 import { dispatcher } from '../../helpers/actions';
 
 export const FETCH_USERS = 'fetch_users';
+export const FETCH_CURRENT_USER = 'fetch_current_user';
+export const FETCH_ADMINS = 'fetch_admins';
+
 export const fetchUsers: ThunkActionCreator<User[]> =
   () => async (dispatch, getState, api) =>
   dispatcher<User[]>(dispatch, FETCH_USERS)(api.get('/users'));
 
-export const FETCH_CURRENT_USER = 'fetch_current_user';
-export const fetchCurrentUser: ThunkActionCreator<Auth> = () => async (dispatch, getState, api) => {
-  const res = await api.get('/current_user');
+export const fetchCurrentUser: ThunkActionCreator<Auth> =
+  () => async (dispatch, getState, api) =>
+  dispatcher<Auth[]>(dispatch, FETCH_CURRENT_USER)(api.get('/current_user'));
 
-  dispatch({
-    type: FETCH_CURRENT_USER,
-    payload: res,
-  });
-};
-
-export const FETCH_ADMINS = 'fetch_admins';
 export const fetchAdmins: ThunkActionCreator<User[]> =
   () => async (dispatch, getState, api) =>
   dispatcher<User[]>(dispatch, FETCH_ADMINS)(api.get('/admins'));
