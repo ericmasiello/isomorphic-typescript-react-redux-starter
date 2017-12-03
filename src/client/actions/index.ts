e75f7e5970c60bfde2b76364bad29f836685ea22
Eric Masiello <eric.j.masiello@gmail.com>
2017-12-03 15:04:20 -0500

    Refactor action to use await-to-js

--- src/client/actions/index.ts
+++ src/client/actions/index.ts
@@ -1,21 +1,23 @@
-
+import to from 'await-to-js';
 import { ThunkActionCreator } from '../../types.d';
 
 export const FETCH_USERS = 'fetch_users';
 export const fetchUsers: ThunkActionCreator<User[]> = () => async (dispatch, getState, api) => {
-  try {
-    const res = await api.get('/users');
-    dispatch({
-      type: FETCH_USERS,
-      payload: res,
-    });
-  } catch (err) {
+  const [err, res] = await to(api.get('/users'));
+
+  if (err) {
     dispatch({
       type: FETCH_USERS,
       payload: err,
       error: true,
     });
+    return;
   }
+
+  dispatch({
+    type: FETCH_USERS,
+    payload: res,
+  });
 };
 
 export const FETCH_CURRENT_USER = 'fetch_current_user';
