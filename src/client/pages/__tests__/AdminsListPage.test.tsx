72699b2370a7f5b6599afecfa40128989145623c
Eric Masiello <eric.j.masiello@gmail.com>
2017-12-04 08:46:20 -0500

    Update tests for Admins and Users pages

--- src/client/pages/__tests__/AdminsListPage.test.tsx
+++ src/client/pages/__tests__/AdminsListPage.test.tsx
@@ -1,25 +1,32 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
-import { ThunkAction } from 'redux-thunk';
-import { Store } from 'redux';
-import * as admins from '../AdminsListPage';
-import { fetchUsers } from '../../actions';
-jest.mock('../../actions/', () => {
-  return {
-    fetchAdmins: jest.fn(() => 'fetching admins'),
-  };
-});
+import { shallow, mount } from 'enzyme';
+import thunk, { ThunkAction } from 'redux-thunk';
+import { Store, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import adminDefault, { AdminsListPage } from '../AdminsListPage';
+import { fetchAdmins } from '../../actions';
+jest.mock('../../actions/');
 
-const { default: { loadData }, AdminsListPage } = admins;
+const mockAction = {
+  type: 'testing',
+  payload: 'fetching admins',
+};
+const mockFetchAdmins = jest.fn(() => mockAction);
+(fetchAdmins as any).mockImplementation(mockFetchAdmins);
+
+const {
+  loadData,
+  component: ConnectedAdminsListPage,
+} = adminDefault;
+
+const mockAdmins: User[] = [{ id: '123', name: 'Admin Eric' }, { id: '456', name: 'Admin Hyun' }];
 
 describe('AdminsListPage', () => {
-  const fetchAdmins = jest.fn(() => () => {}) as () => ThunkAction<Promise<User[]>, User[], {}>;
-  const admins: User[] = [{ id: '123', name: 'Eric' }];
   test('should render', () => {
     const wrapper = shallow(
       <AdminsListPage
-        fetchAdmins={fetchAdmins}
-        admins={admins}
+        fetchAdmins={fetchAdmins as () => ThunkAction<Promise<User[]>, User[], {}>}
+        admins={mockAdmins}
       />,
     );
 
@@ -33,6 +40,27 @@ describe('loadData', () => {
     store.dispatch = jest.fn();
 
     loadData(store);
-    expect(store.dispatch).toHaveBeenCalledWith('fetching admins');
+    expect(store.dispatch).toHaveBeenCalledWith(mockAction);
+  });
+});
+
+describe('ConnectedAdminsListPage', () => {
+  const storeMocker = (admins: User[] = []) => {
+    const mockReducer = (state: any = { admins }) => state;
+    return createStore(
+      mockReducer,
+      applyMiddleware(thunk),
+    );
+  };
+
+  test('should render admins passed via store', () => {
+    const wrapper = mount(
+      <Provider store={storeMocker(mockAdmins)}>
+        <ConnectedAdminsListPage />
+      </Provider>,
+    );
+
+    expect(wrapper.contains(<li>Admin Eric</li>)).toBe(true);
+    expect(wrapper.contains(<li>Admin Hyun</li>)).toBe(true);
   });
 });
--- src/client/pages/__tests__/UsersListPage.test.tsx
+++ src/client/pages/__tests__/UsersListPage.test.tsx
@@ -1,28 +1,48 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
-import { ThunkAction } from 'redux-thunk';
-import { Store } from 'redux';
-import * as users from '../UsersListPage';
-jest.mock('../../actions/', () => {
-  return {
-    fetchUsers: jest.fn(() => 'fetching users'),
-  };
-});
+import { shallow, mount } from 'enzyme';
+import thunk, { ThunkAction } from 'redux-thunk';
+import { Store, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import userDefault, { UsersListPage } from '../UsersListPage';
+import { fetchUsers } from '../../actions';
+jest.mock('../../actions/');
 
-const { default: { loadData }, UsersListPage } = users;
+const mockAction = {
+  type: 'testing',
+  payload: 'fetching users',
+};
+const mockFetchUsers = jest.fn(() => mockAction);
+
+(fetchUsers as any).mockImplementation(mockFetchUsers);
+
+const {
+  loadData,
+  component: ConnectedUsersListPage,
+} = userDefault;
+
+const mockUsers: User[] = [{ id: '123', name: 'Eric' }, { id: '456', name: 'Hyun' }];
 
 describe('UsersListPage', () => {
-  const fetchUsers = jest.fn(() => () => {}) as () => ThunkAction<Promise<User[]>, User[], {}>;
-  const users: User[] = [{ id: '123', name: 'Eric' }];
   test('should render', () => {
     const wrapper = shallow(
       <UsersListPage
-        fetchUsers={fetchUsers}
-        users={users}
+        fetchUsers={(fetchUsers as () => ThunkAction<Promise<User[]>, User[], {}>)}
+        users={mockUsers}
       />,
     );
 
     expect(wrapper).toHaveLength(1);
+  });
+
+  test('should render number of users loaded in title', () => {
+    const wrapper = shallow(
+      <UsersListPage
+        fetchUsers={(fetchUsers as () => ThunkAction<Promise<User[]>, User[], {}>)}
+        users={mockUsers}
+      />,
+    );
+
+    expect(wrapper.contains(<title>2 Users Loaded</title>)).toBe(true);
   });
 });
 
@@ -32,6 +52,27 @@ describe('loadData', () => {
     store.dispatch = jest.fn();
 
     loadData(store);
-    expect(store.dispatch).toHaveBeenCalledWith('fetching users');
+    expect(store.dispatch).toHaveBeenCalledWith(mockAction);
+  });
+});
+
+describe('ConnectedUsersListPage', () => {
+  const storeMocker = (users: User[] = []) => {
+    const mockReducer = (state: any = { users }) => state;
+    return createStore(
+      mockReducer,
+      applyMiddleware(thunk),
+    );
+  };
+
+  test('should render users passed via store', () => {
+    const wrapper = mount(
+      <Provider store={storeMocker(mockUsers)}>
+        <ConnectedUsersListPage />
+      </Provider>,
+    );
+
+    expect(wrapper.contains(<li>Eric</li>)).toBe(true);
+    expect(wrapper.contains(<li>Hyun</li>)).toBe(true);
   });
 });
