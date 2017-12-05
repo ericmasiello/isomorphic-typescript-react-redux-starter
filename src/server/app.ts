aea0280703002b555bb68bd7a23d7a354049b78a
Eric Masiello <eric.j.masiello@gmail.com>
2017-12-05 08:41:12 -0500

    Remove unused imports

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,6 +1,4 @@
 import * as express from 'express';
-import { ThunkAction } from 'redux-thunk';
-import { AxiosInstance } from 'axios';
 import apiRouter from './routes/api';
 import uiRouter from './routes/ui';
 
