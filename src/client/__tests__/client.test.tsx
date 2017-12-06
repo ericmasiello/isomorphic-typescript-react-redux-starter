445d1e1529b94e61f0e0fcff82bf4eee1b8389a7
Eric Masiello <eric.j.masiello@gmail.com>
2017-12-05 21:10:42 -0500

    Add test for client.tsx

---
+++ src/client/__tests__/client.test.tsx
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+import * as ReactDOM from 'react-dom';
+import { client } from '../client';
+jest.mock('react-dom');
+
+test('should call hydrate', () => {
+  expect(ReactDOM.hydrate).toBeCalledWith(
+    client,
+    document.querySelector('#root'),
+  );
+});
